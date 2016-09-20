3e549b66e9960d0bdd572194851c9e7be8bbfed6
Gaspard WITRAND <gwitrand@student.42.fr>
2016-09-20 14:15:43 +0200

    init

---
+++ ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwitrand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/08 16:57:50 by gwitrand          #+#    #+#             */
+/*   Updated: 2016/09/08 16:58:54 by gwitrand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FT_H
+# define __FT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+void	ft_swap(int *a, int *b);
+#endif
